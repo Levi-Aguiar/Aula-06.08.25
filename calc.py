0c0e92955c1d0ab395c7bed20280a38e1b3b81dd
Levi-Aguiar <Levi.jaguiar@gmail.com>
2025-08-06 19:39:22 -0300

    Adicionando Menu

---
+++ calc.py
@@ -0,0 +1,10 @@
+while True:
+    print("Bem vindo a calculadora.")
+    print("Selecione uma das opções abaixo para prosseguir.")
+    print("1- Soma")
+    print("2- Subtração")
+    print("3- Multiplicação")
+    print("4- Divisão")
+    print("5- Sair")
+
+    opcao = input("Digite o número da opção desejada:")
